3eb411f0f78212e2a6937f79b0319fd4cdb3c366
Parth <pgalani193@rku.ac.in>
2020-10-06 14:53:45 +0530

    Add MyUtil.java in API Url and MainActivity.java Create jsonArrayRequest using volley library...

--- app/src/main/java/com/rku/tutorial12/MainActivity.java
+++ app/src/main/java/com/rku/tutorial12/MainActivity.java
@@ -1,14 +1,68 @@
 package com.rku.tutorial12;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    RequestQueue requestQueue;
+    JsonArrayRequest jsonArrayRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        requestNetworkCall();
+    }
+
+    private void requestNetworkCall() {
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                MyUtil.URL_USER,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        MyUtil.jsonArray = response;
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(jsonArrayRequest);
     }
 }
---
+++ app/src/main/java/com/rku/tutorial12/MyUtil.java
@@ -0,0 +1,8 @@
+package com.rku.tutorial12;
+
+import org.json.JSONArray;
+
+public class MyUtil {
+    public static final String URL_USER = "https://jsonplaceholder.typicode.com/users";
+    public static JSONArray jsonArray = null;
+}
